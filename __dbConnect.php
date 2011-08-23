2455fc8aa85fc540215347c5db66ac02f7c4ff02
jlagarto <jlagarto@igc.gulbenkian.pt>
2011-08-23 11:40:57 +0100

    dbconnect.php path updated

--- __dbConnect.php
+++ __dbConnect.php
@@ -6,7 +6,7 @@ error_reporting(1);
 if(isset($_SESSION['path']) or $_SESSION['path']!=""){
 	//use $_SERVER['DOCUMENT_ROOT'] with requistitions and animalhouse
 	//echo $_SERVER['DOCUMENT_ROOT'].$_SESSION['path']."/.htconnect.php";
-	require_once $_SERVER['DOCUMENT_ROOT'].$_SESSION['path']."/.htconnect.php";
+	require_once $_SESSION['path']."/.htconnect.php";
 } else {
 	echo "Session was lost! Waiting for automatic redirection...";
 	echo "<meta HTTP-EQUIV='REFRESH' content='3; url=../'>";
